2f130f374f04ebfad0290b898e114bfb14d088e4
davidmanhtrinh-tech <davidmanhtrinh@gmail.com>
2026-08-19 15:59:15 +1000

    added in css and js - inputted js edge
    configuring js edge scripts

--- main.js
+++ main.js
@@ -4,12 +4,22 @@ const introDialog = document.getElementById("intro-dialogue");
 //Locating the close button
 const introCloseButton = document.getElementById("intro-dialog-close");
 //Show the found element in our browser modals//
-//console.log(introDialog);
+////console.log(introDialog);
 //Find our test button//
 const testButton = document.getElementById("test-button");
+//find my key button for test//
+const key = document.getElementById("key-test");
 
 //init our synth//
-const synth = new Tone.Synth().toDestination();
+const synth = new Tone.PolySynth().toDestination();
+//is the user currently holding down the key
+let mouseButtonHeld = false;
+//if user holds down key, set to truem then if they let it up, set to false
+window.addEventListener("mousedown", function () {
+  mouseButtonHeld = true;
+});
+
+window.addEventListener("mousedown", function () {});
 
 //Dialog logic//
 introDialog.showModal();
@@ -34,5 +44,40 @@ testButton.addEventListener("click", playNote);
 // function that runs when clicked//
 function playNote() {
   // Play a note for a duration//
-  synth.triggerAttackRelease("C4", "8n");
+  synth.triggerAttackRelease("c4", "8n");
 }
+
+function playDataNoteD(e) {
+  let buttonClicked = e.target;
+  //console.log(buttonClicked);
+  let note = buttonClicked.dataset.note;
+  //console.log(e.note);
+  synth.triggerAttackRelease(note, "8n");
+}
+
+function startNote(e) {
+  //find key that was click - so let key pressed
+  let keyPressed = e.target;
+  //find the note associated with the key
+  let note = keyPressed.dataset.note;
+  synth.triggerAttackRelease(note);
+}
+
+function endNote(e) {
+  let keyPressed = e.target;
+  let note = keyPressed.dataset.note;
+  synth.triggerAttackRelease(note);
+}
+
+key.addEventListener("mousedown", startNote);
+key.addEventListener("mouseup", endNote);
+key.addEventListener("mouseleave", endNote);
+//if user is holding mouse button
+key.addEventListener("mouseenter", function (e) {
+  if (mouseButtonHeld === true) {
+    startNote(e);
+  }
+});
+
+//key.addEventListener("click", playNoteD);
+//testButton.addEventListener("click", playDataNoteD);
